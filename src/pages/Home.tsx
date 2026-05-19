a4acd423c324ba3ca187a25cf9bb2d3c1e08460f
Ricardo M Carvalho <ricardo.m.carvalho@tripleoak.vc>
2026-05-19 15:56:34 +0100

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -332,7 +332,7 @@ export default function Home() {
               position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)',
               zIndex: 2, display: 'flex', alignItems: 'center', justifyContent: 'center',
             }}>
-              <img src="/Logo_sem_letras.png" alt="NIEUSYNC" style={{ width: '400px', height: '400px', objectFit: 'contain' }} />
+              <img src="/Logo_sem_letras.png" alt="NIEUSYNC" style={{ width: '800px', height: '800px', objectFit: 'contain' }} />
             </div>
 
       
